550b6c5320cf94eaf7ed3f05abf1ea2a6e42a844
Dav-Did <115046188+Dav-Did@users.noreply.github.com>
2024-03-14 23:51:47 -0700

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.module5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.module5 to javafx.fxml;
-    exports com.example.module5;
-}
